88894bedc5a54512250dd265d7f2180a8dee11f1
Demir Khawaja <khawajad7828@my.sduhsd.net>
2019-12-19 11:45:43 -0800

    Third Commit

---
+++ sizeofvariables.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+
+char b = 23;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", b, sizeof(b));
+
+  float c = 5.45;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", c, sizeof(c));
+
+  double d = 8;
+
+  // print value and size of an int variable
+  printf("int a value: %d and size: %d bytes\n", d, sizeof(d));
+}
+
